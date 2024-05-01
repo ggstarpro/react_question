cdbf465850fe398c35a90a1af0090c1637a518b7
GentaMorino <genta.morino@gmail.com>
2024-05-01 22:28:39 +0900

    問題2 カウントアップ作製(セクション3 useState, カスタムフック)

---
+++ src/hooks/useCount.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+type UseCountUp = () => {
+  count: number;
+  onClickCountUp: () => void;
+};
+
+export const useCountUp: UseCountUp = () => {
+  const [count, setCount] = useState(0);
+  const onClickCountUp = () => {
+    setCount((prevState) => prevState + 1);
+    // setCount((prevState) => prevState + 1)
+
+    // setCount(count + 1)
+    // setCount(count + 1)
+  };
+  return {
+    count,
+    onClickCountUp,
+  };
+};
--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from 'next';
 
-import ThreeBoxContent from '@/components/ThreeBoxContent';
+import Button from '@/components/common/parts/Button';
+import { useCountUp } from '@/hooks/useCount';
+
 const Page: NextPage = () => {
+  const { count, onClickCountUp } = useCountUp();
   return (
     <>
       <div className="mx-auto mt-10 max-w-4xl">
+        {/* Q1
         <ThreeBoxContent
           title1="これはタイトルです"
           content1="これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。"
@@ -12,7 +16,14 @@ const Page: NextPage = () => {
           content2="これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。"
           title3="これはタイトルです"
           content3="これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。これはコンテンツです。"
-        />
+        /> */}
+
+        <div className="flex justify-center">
+          <div>
+            <h3 className="mb-4 text-center text-6xl">{count}</h3>
+            <Button variant="primary" label="カウントアップ" onClick={onClickCountUp} />
+          </div>
+        </div>
       </div>
     </>
   );
