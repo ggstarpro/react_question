dc29e035a508bdcb8b9cebb6426d8ef89455d739
GentaMorino <genta.morino@gmail.com>
2024-05-03 19:58:11 +0900

    問題6 フィードバック一覧表示 (map)

--- src/pages/practice/01.tsx
+++ src/pages/practice/01.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next';
 
 import Button from '@/components/common/parts/Button';
-import { COLORS, useChangeBgColor } from '@/hooks/useChangeBgColor';
+import { useChangeBgColor } from '@/hooks/useChangeBgColor';
 import { useCountUp } from '@/hooks/useCount';
+import { useDisplayFeedback } from '@/hooks/useDisplayFeedback';
 import { useRealTimeText } from '@/hooks/useRealTimeText';
 import { useTextShowHidden } from '@/hooks/useTextShowHidden';
 
@@ -15,7 +16,9 @@ const Page: NextPage = () => {
   const { text, handleChangeText } = useRealTimeText();
   /** Q5 */
   const { bgColorIndex, handleChangeBgColor } = useChangeBgColor();
-
+  /** Q6 */
+  const { feedBackText, feedBackList, handleChangeFeedBackText, handleSendFeedBack } =
+    useDisplayFeedback();
   return (
     <>
       <div className="mx-auto mt-10 max-w-4xl">
@@ -57,9 +60,30 @@ const Page: NextPage = () => {
             </div>
           </div>
         </div> */}
+
+        {/* Q5
         <div className="h-screen pt-8" style={{ backgroundColor: COLORS[bgColorIndex] }}>
           <div className="flex justify-center">
             <Button variant="primary" label="色を変更" onClick={handleChangeBgColor} />
+          </div>
+        </div> */}
+
+        <div className="flex justify-center">
+          <div>
+            <textarea
+              className="rounded-sm border px-3 py-2"
+              onChange={handleChangeFeedBackText}
+              placeholder="フィードバックを入力してください"
+              value={feedBackText}
+            ></textarea>
+            <div className="mt-4 flex justify-center">
+              <Button variant="primary" label="送信する" onClick={handleSendFeedBack} />
+            </div>
+            <div className="mt-2">
+              {feedBackList.map((feedBack, index) => (
+                <li key={index}>{feedBack}</li>
+              ))}
+            </div>
           </div>
         </div>
       </div>
